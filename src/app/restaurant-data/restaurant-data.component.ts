942f01aa23b0a3467f0cce3cabe6a46abc779df1
goldenfox <behnam.bobvv@gmail.com>
2019-07-06 09:39:57 +0430

    restaurant detail page modified

--- src/app/restaurant-data/restaurant-data.component.ts
+++ src/app/restaurant-data/restaurant-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantDataService } from '../restaurant-data.service';
 
 @Component({
   selector: 'app-restaurant-data',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class RestaurantDataComponent implements OnInit {
   restaurantName = "باگت";
   average_rate = 4.7;
+  data = {};
   categories = "ساندویچ • فست فود • برگر • پیتزا • سالاد • غذای ایرانی";
   address = "تهران، پاسداران، بالا تر از برج سفید، نبش بوستان دهم";
-  constructor() { }
+  constructor(restaurantData : RestaurantDataService) {
+    restaurantData.getRestaurants(1).subscribe(
+      data => { this.data = data},
+      err => console.error(err),
+      () => console.log('done loading foods ' + this.data)
+    );
+  }
 
   ngOnInit() {
   }
